4fb7a66b53288f6b48646643d3595e270fefa7f2
Shuhao Wang <swang@watertrax.local>
2016-08-24 10:57:40 -0700

    add simple header

---
+++ src/app/layout/header/simple-header.directive.js
@@ -0,0 +1,21 @@
+/**
+ * Created by swang on 8/24/2016.
+ */
+(function () {
+  'use strict';
+
+  angular
+    .module('testProject')
+    .directive('simpleHeader', simpleHeader);
+
+
+  /* @ngInject */
+  function simpleHeader(){
+    return {
+      templateUrl :'/app/layout/header/simple-header.html'
+    }
+  }
+
+})();
+
+
